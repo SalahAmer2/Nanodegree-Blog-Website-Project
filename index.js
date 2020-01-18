15f9898dde1f4cf7b4d33654bebd83e9c4936066
Salah Amer <Salah@example.com>
2020-01-18 14:18:06 +0200

    third commit

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ app.post("/subscribed-successfully", function(req, res){
   res.sendFile(__dirname + "/html/subscribed-successfully.html");
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
